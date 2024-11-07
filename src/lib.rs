f88c9ec97d8d1d75d8a9829eca0462d46724cbc6
jiacai2050 <dev@liujiacai.net>
2024-11-08 00:01:57 +0800

    try build

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,32 @@
+use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
+use vsag_sys as sys;
+
+#[pyclass]
+struct Index {
+    core: sys::VsagIndex,
+}
+
+#[pymethods]
+impl Index {
+    #[new]
+    fn try_new(index_type: &str, params: &str) -> PyResult<Self> {
+        sys::VsagIndex::new(index_type, params)
+            .map_err(|e| PyValueError::new_err(e.message))
+            .map(|core| Self { core })
+    }
+}
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
 fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+    Ok((a * b).to_string())
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn vsag(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_class::<Index>()?;
     Ok(())
 }
