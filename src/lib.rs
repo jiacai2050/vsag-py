b5535d9166ed64d6e5375edfb606bedc2ddb4f37
jiacai2050 <dev@liujiacai.net>
2024-11-11 16:58:37 +0800

    bump version

--- src/lib.rs
+++ src/lib.rs
@@ -49,16 +49,9 @@ impl Index {
     }
 }
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a * b).to_string())
-}
-
-/// A Python module implemented in Rust.
+/// VSAG Python module implemented in Rust.
 #[pymodule]
 fn vsag(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     m.add_class::<Index>()?;
     Ok(())
 }
